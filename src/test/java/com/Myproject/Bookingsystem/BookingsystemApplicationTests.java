7c773f4fbc198888fd72fb4a11e486b9bccefe48
spsankar02 <159914816+spsankar02@users.noreply.github.com>
2024-02-23 18:13:03 +0530

    Add files via upload

---
+++ src/test/java/com/Myproject/Bookingsystem/BookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Myproject.Bookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
